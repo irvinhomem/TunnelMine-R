0227105235841ae5951f9ad904e46c7548a9e80f
Irvin Homem <irvinhomem@gmail.com>
2016-10-21 22:06:56 +0200

    Cleaning up code ... adding comments

--- PreProcessing.R
+++ PreProcessing.R
@@ -4,21 +4,24 @@
 library("caret")
 library("rpart")
 
-#http_flds <- createFolds(http_ov_dns_pcap_features_df, k = 10, list = FALSE, times = 10)
-
+# Choosing the number of features to use in the different classifiers
 #frmla = as.formula(proto_label ~ avg_entropy + avg_ip_req_len + avg_dns_req_len)  # With 3 features
 frmla = as.formula(proto_label ~ avg_entropy + avg_ip_req_len)                   # With 2 features
 #frmla = as.formula(proto_label ~ avg_entropy)                   # With 1 feature
 
-
-#train_ctrl <- trainControl(method = "cv", number = 10, p=0.9, savePredictions = TRUE, returnResamp = TRUE, classProbs = TRUE) # classProbs = TRUE # sampling = "none"
-#train_ctrl <- trainControl(method = "cv", number = 10, p=0.9, savePredictions = "final", returnResamp = TRUE, classProbs = TRUE) # classProbs = TRUE # sampling = "none"
-# Best so far, but results in different folds every time a new classifcation algorithm is tried (i.e. decision trees, knn, etc)
-#train_ctrl <- trainControl(method = "cv", number = 10, p=0.9, savePredictions = "final", returnResamp = "final", returnData = TRUE) # classProbs = TRUE # sampling = "none"
-#myFolds <- createFolds(json_features_all_pcaps_df$avg_entropy, k = 10, returnTrain = FALSE)
-#myFolds <- createDataPartition(json_features_all_pcaps_df$avg_entropy, times = 10, p = 0.9, list = FALSE)
-myFolds <- createDataPartition(json_features_all_pcaps_df$proto_label, times = 10, p = 0.9)
+# Setting up pre-defined (fixed) folds/resamples --> in this case where k=10 folds (for testing) and the amount of data used for training (p = 0.9) i.e 90%
+#myFolds <- createDataPartition(json_features_all_pcaps_df$avg_entropy, times = 10, p = 0.9, list = FALSE)    # The class probability and distribution got skewed here
+myFolds <- createDataPartition(json_features_all_pcaps_df$proto_label, times = 10, p = 0.9)   # The class probability distributions in the folds were better here
 #myFolds
 head(myFolds)
+
+
+# Best so far, but results in different folds every time a new classifcation algorithm is tried (i.e. decision trees, knn, etc)
+#train_ctrl <- trainControl(method = "cv", number = 10, p=0.9, savePredictions = "final", returnResamp = "final", returnData = TRUE) # classProbs = TRUE # sampling = "none"
+
+# Using predefined folds/resamples and Setting the parameters for the cross-validation / evaluation of the training model
+# Using "k-fold Cross-Validation" where the k-folds are set up previously and FIXED
 train_ctrl <- trainControl(method = "cv", index = myFolds, savePredictions = "final", returnResamp = "final", returnData = TRUE) # classProbs = TRUE # sampling = "none"
+
+# Using Leave-One-Out-Cross-Validation (LOOCV)
 #train_ctrl <- trainControl(method = "loocv", index = myFolds, savePredictions = "final", returnResamp = "final", returnData = TRUE)
