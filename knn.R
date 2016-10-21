2dde1f88395dea52236081df3846a55c43a77b67
Irvin Homem <irvinhomem@gmail.com>
2016-10-21 21:39:59 +0200

    Attempt at modularizing code: Created file where k-Nearest Neighbour classification code is maintained

---
+++ knn.R
@@ -0,0 +1,19 @@
+#======
+# k-NN (k-Nearest Neighbours)
+#======
+#"preProcess" in order to normalize with "center" and "scale" (something like z-score-normalization)
+#“center“: subtract mean from values.
+#“scale“: divide values by standard deviation.
+#knnFit <- train(frmla, data=json_features_all_pcaps_df, method = "knn", trControl=train_ctrl, preProcess = c("center", "scale"), tuneLength = 10)
+# 1-30 NN
+knnFit <- train(frmla, data=json_features_all_pcaps_df, method = "knn", trControl=train_ctrl, preProcess = c("center", "scale"), tuneGrid = expand.grid(.k=1:30))
+# only 1-NN
+#knnFit <- train(frmla, data=json_features_all_pcaps_df, method = "knn", trControl=train_ctrl, preProcess = c("center", "scale"), tuneGrid = expand.grid(.k=1)) 1NN
+
+plot(knnFit)
+knnFit
+knnFit$finalModel
+knnFit$results
+knnFit$bestTune
+knnFit$pred[order(knnFit$pred$Resample, knnFit$pred$rowIndex),]
+confusionMatrix(knnFit)
