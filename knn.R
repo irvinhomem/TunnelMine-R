d8d8c9b00f19213a543da5f68e842479a90c8379
Irvin Homem <irvinhomem@gmail.com>
2016-10-22 16:32:51 +0200

    Backing up ...

--- knn.R
+++ knn.R
@@ -1,14 +1,15 @@
+
 #======
 # k-NN (k-Nearest Neighbours)
 #======
-#"preProcess" in order to normalize with "center" and "scale" (something like z-score-normalization)
-#“center“: subtract mean from values.
-#“scale“: divide values by standard deviation.
+# "preProcess" in order to normalize with "center" and "scale" (something like z-score-normalization)
+# “center“: subtract mean from values.
+# “scale“: divide values by standard deviation.
 #knnFit <- train(frmla, data=json_features_all_pcaps_df, method = "knn", trControl=train_ctrl, preProcess = c("center", "scale"), tuneLength = 10)
 # 1-30 NN
-knnFit <- train(frmla, data=json_features_all_pcaps_df, method = "knn", trControl=train_ctrl, preProcess = c("center", "scale"), tuneGrid = expand.grid(.k=1:30))
+#knnFit <- train(frmla, data=json_features_all_pcaps_df, method = "knn", trControl=train_ctrl, preProcess = c("center", "scale"), tuneGrid = expand.grid(.k=1:30))
 # only 1-NN
-#knnFit <- train(frmla, data=json_features_all_pcaps_df, method = "knn", trControl=train_ctrl, preProcess = c("center", "scale"), tuneGrid = expand.grid(.k=1)) 1NN
+knnFit <- train(frmla, data=json_features_all_pcaps_df, method = "knn", trControl=train_ctrl, preProcess = c("center", "scale"), tuneGrid = expand.grid(.k=1)) #1-NN
 
 plot(knnFit)
 knnFit
@@ -17,3 +18,5 @@ knnFit$results
 knnFit$bestTune
 knnFit$pred[order(knnFit$pred$Resample, knnFit$pred$rowIndex),]
 confusionMatrix(knnFit)
+
+
