d121bbc1eae47ef9ae9cf6c99c00e3a59b2fe425
Irvin Homem <irvinhomem@gmail.com>
2016-10-25 00:29:59 +0200

    Backing up

--- LearningVectorQuantization.R
+++ LearningVectorQuantization.R
@@ -5,7 +5,8 @@ lvqModel <- train(frmla, data=json_features_all_pcaps_df, method = "lvq", trCont
 #lvqModel <- train(frmla, data=json_features_all_pcaps_df, method = "lvq", trControl=train_ctrl, preProcess = c("center", "scale"), tuneLength = 10)
 
 #tuningGrid <- expand.grid(size=c(5,10,15,20,25,30), k=c(1,2,3,4,5,6))
-#lvqModel <- train(frmla, data=json_features_all_pcaps_df, method = "lvq", trControl=train_ctrl, preProcess = c("center", "scale"), tuneGrid = tuningGrid)
+tuningGrid <- expand.grid(size=c(3,6,9,12,15,18), k=1:20)
+lvqModel <- train(frmla, data=json_features_all_pcaps_df, method = "lvq", trControl=train_ctrl, preProcess = c("center", "scale"), tuneGrid = tuningGrid)
 
 plot(lvqModel)
 
