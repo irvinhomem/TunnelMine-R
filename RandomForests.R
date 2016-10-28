2238d837f1fc2058e35a7ec29844ac3c62ce490a
Irvin Homem <irvinhomem@gmail.com>
2016-10-28 17:41:51 +0200

    Testing lots of parameter tuning options for Random Forests. In the end it's not so useful because there are only 3 predictors(features) max in this dataset. Thus, Random Forests are not so useful. They are usually useful for datasets with lots of features (columns / predictors).

--- RandomForests.R
+++ RandomForests.R
@@ -5,14 +5,42 @@ library(caret)
 # Ensure to check the number of predictors to be used (in PreProcessing) --> Fixed number or Random choice
 # NB: Takes a bit of a while with "Random Choice" of parameters
 
-#RF_model <- train(frmla, data=json_features_all_pcaps_df, method = "rf", trControl=train_ctrl, preProcess = c("center", "scale"), tuneGrid = expand.grid(.k=1:30))
+#RF_model <- train(frmla, data=json_features_all_pcaps_df, method = "rf", trControl=train_ctrl, preProcess = c("center", "scale"))
 
-#RF_model <- train(frmla, data=json_features_all_pcaps_df, method = "rf", trControl=train_ctrl)
+RF_model <- train(frmla, data=json_features_all_pcaps_df, method = "rf", trControl=train_ctrl)
 
-#RF_model <- train(frmla, data=json_features_all_pcaps_df, method = "rf", trControl=train_ctrl, tuneLength = 15)
+# Best so far: Nice looking graph :-)
+RF_model <- train(frmla, data=json_features_all_pcaps_df, method = "rf", trControl=train_ctrl, tuneLength = 15)
 
+# mtry: Number of variables randomly sampled as candidates at each split.
+# ntree: Number of trees to grow. (only in "RandomForest" package, not in Caret)
+#metric_param <- "Accuracy"
+#metric_param <- "ROC"
+#Not so good
+##RF_model <- train(frmla, data=json_features_all_pcaps_df, method = "rf", trControl=train_ctrl, tuneGrid = expand.grid(.mtry=c(1:15)))
+# Better (But takes rather long if allowed to choose random features)
 RF_model <- train(frmla, data=json_features_all_pcaps_df, method = "rf", trControl=train_ctrl, tuneGrid = expand.grid(.mtry=c(1:30)))
-#warnings()
+
+#RF_model <- train(frmla, data=json_features_all_pcaps_df, method = "rf", trControl=train_ctrl, tuneGrid = expand.grid(.mtry=c(1:20)))
+
+# Default ntree = 500
+#RF_model <- train(frmla, data=json_features_all_pcaps_df, method = "rf", trControl=train_ctrl, ntree=300, tuneGrid = expand.grid(.mtry=c(1:20)))
+#RF_model <- train(frmla, data=json_features_all_pcaps_df, method = "rf", trControl=train_ctrl, ntree=100, tuneGrid = expand.grid(.mtry=c(1:30)))
+#RF_model <- train(frmla, data=json_features_all_pcaps_df, method = "rf", trControl=train_ctrl, ntree=100, tuneGrid = expand.grid(.mtry=c(1:15)))
+RF_model <- train(frmla, data=json_features_all_pcaps_df, method = "rf", trControl=train_ctrl, ntree=300, tuneGrid = expand.grid(.mtry=c(1:3)))
+#RF_model <- train(frmla, data=json_features_all_pcaps_df, method = "rf", trControl=train_ctrl, ntree=100, tuneGrid = expand.grid(.mtry=c(1:5)))
+
+#RF_model <- train(frmla, data=json_features_all_pcaps_df, method = "rf", trControl=train_ctrl, ntree=300)
+#RF_model <- train(frmla, data=json_features_all_pcaps_df, method = "rf", trControl=train_ctrl, ntree=100)
+#RF_model <- train(frmla, data=json_features_all_pcaps_df, method = "rf", trControl=train_ctrl, ntree=50)
+#RF_model <- train(frmla, data=json_features_all_pcaps_df, method = "rf", trControl=train_ctrl, ntree=10)
+#RF_model <- train(frmla, data=json_features_all_pcaps_df, method = "rf", trControl=train_ctrl, ntree=5)
+#RF_model <- train(frmla, data=json_features_all_pcaps_df, method = "rf", trControl=train_ctrl, ntree=2)
+
+warnings()
+
+#head(RF_model$terms)
+head(RF_model$trainingData)
 
 plot(RF_model)
 
