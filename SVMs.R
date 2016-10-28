e78049ab97d0cba7902aaba53ac2728000a97d7f
Irvin Homem <irvinhomem@gmail.com>
2016-10-28 19:43:30 +0200

    Added SVMs with different kernels to try out classification. Also, added line to ensure that confusionMatrix function is picked from Caret and nowhere else since RSNNS package for Perceptrons causes problems by "masking it".

---
+++ SVMs.R
@@ -0,0 +1,32 @@
+library(caret)
+library(kernlab)
+library(e1071)
+library(LiblineaR)
+
+#------------------
+# Using SVMs - Support Vector Machines
+#==================
+
+svm_model <- train(frmla, data=json_features_all_pcaps_df, method = "svmRadial", trControl=train_ctrl)
+#svm.model <- train(frmla, data=json_features_all_pcaps_df, method = "svmRadial", metric="ROC", trControl=train_ctrl) # Results in errors
+
+#svm.model <- train(frmla, data=json_features_all_pcaps_df, method = "svmLinear", trControl=train_ctrl)
+#svm.model <- train(frmla, data=json_features_all_pcaps_df, method = "lssvmLinear", trControl=train_ctrl)
+
+svm_model <- train(frmla, data=json_features_all_pcaps_df, method = "svmLinear2", trControl=train_ctrl)
+
+svm_model <- train(frmla, data=json_features_all_pcaps_df, method = "svmPoly", trControl=train_ctrl)
+
+
+
+# Results
+plot(svm.model)
+
+svm_model$pred[order(svm_model$pred$Resample, svm_model$pred$rowIndex),]
+svm_model
+svm_model$finalModel
+svm_model$results
+svm_model$bestTune
+
+caret::confusionMatrix(svm_model)
+#confusionMatrix(svm_model$pred[order(svm_model$pred$Resample, svm_model$pred$rowIndex),])
