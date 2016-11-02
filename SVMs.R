3182a04920b5225d3ddcd80844acc85635e0e1d8
Irvin Homem <irvinhomem@gmail.com>
2016-11-02 19:38:40 +0100

    Tested more Linear SVM algorithms and added confusion matrix with performance measures (Precision, Recall)

--- SVMs.R
+++ SVMs.R
@@ -10,10 +10,9 @@ library(LiblineaR)
 svm_model <- train(frmla, data=json_features_all_pcaps_df, method = "svmRadial", trControl=train_ctrl)
 #svm.model <- train(frmla, data=json_features_all_pcaps_df, method = "svmRadial", metric="ROC", trControl=train_ctrl) # Results in errors
 
-#svm.model <- train(frmla, data=json_features_all_pcaps_df, method = "svmLinear", trControl=train_ctrl)
-#svm.model <- train(frmla, data=json_features_all_pcaps_df, method = "lssvmLinear", trControl=train_ctrl)
-
-svm_model <- train(frmla, data=json_features_all_pcaps_df, method = "svmLinear2", trControl=train_ctrl)
+svm.model <- train(frmla, data=json_features_all_pcaps_df, method = "svmLinear", trControl=train_ctrl) # Best results also
+#svm.model <- train(frmla, data=json_features_all_pcaps_df, method = "lssvmLinear", trControl=train_ctrl) # Takes long ... results in some errors
+svm_model <- train(frmla, data=json_features_all_pcaps_df, method = "svmLinear2", trControl=train_ctrl) # Best So Far for SVMs
 
 svm_model <- train(frmla, data=json_features_all_pcaps_df, method = "svmPoly", trControl=train_ctrl)
 
@@ -29,4 +28,5 @@ svm_model$results
 svm_model$bestTune
 
 caret::confusionMatrix(svm_model)
+caret::confusionMatrix(data=svm_model$pred$pred, reference = svm_model$pred$obs)
 #confusionMatrix(svm_model$pred[order(svm_model$pred$Resample, svm_model$pred$rowIndex),])
