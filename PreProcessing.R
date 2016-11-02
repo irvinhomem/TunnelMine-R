1034f16e4777ae561828584454cc099e3baba7ad
Irvin Homem <irvinhomem@gmail.com>
2016-11-02 19:37:47 +0100

    Testing different model formulae and added Repeated-Cross-Validation (repeatedcv) and Leave-One-Out-Cross-Validation (LOOCV)

--- PreProcessing.R
+++ PreProcessing.R
@@ -6,8 +6,8 @@ library("rpart")
 
 # Choosing the number of features to use in the different classifiers
 frmla = as.formula(proto_label ~ avg_entropy + avg_ip_req_len + avg_dns_req_len)     # With 3 features
-#frmla = as.formula(proto_label ~ avg_entropy + avg_ip_req_len)                       # With 2 features
-#frmla = as.formula(proto_label ~ avg_entropy)                                        # With 1 feature
+frmla = as.formula(proto_label ~ avg_entropy + avg_ip_req_len)                       # With 2 features
+frmla = as.formula(proto_label ~ avg_entropy)                                        # With 1 feature
 #frmla = as.formula(proto_label  ~ .)                                                 # Randomly picked features ?? (For Random Forests) (Perhaps avoid this because it picks the filename also)
 frmla = as.formula(proto_label ~ . - filename)                          # Use all features except proto_label and filename (For Random Forests)
 
@@ -16,7 +16,7 @@ frmla
 # Setting up PRE-DEFINED (FIXED) folds/resamples --> in this case where k=10 folds (for testing) and the amount of data used for training (p = 0.9) i.e 90%
 #myFolds <- createDataPartition(json_features_all_pcaps_df$avg_entropy, times = 10, p = 0.9, list = TRUE)    # The class probability and distribution got skewed here
 myFolds <- createDataPartition(json_features_all_pcaps_df$proto_label, times = 10, p = 0.9, list = TRUE)   # The class probability distributions in the folds were better here
-#myFolds
+myFolds
 head(myFolds)
 
 
@@ -28,6 +28,9 @@ head(myFolds)
 train_ctrl <- trainControl(method = "cv", index = myFolds, savePredictions = "final", returnResamp = "final", returnData = TRUE) # classProbs = TRUE # sampling = "none"
 #NB: Setting classProbs = TRUE causes errors for some reason
 
+# Using Repeated-10-Fold-Cross-Validation 
+train_ctrl <- trainControl(method = "repeatedcv", number = 10, p=0.9, repeats = 5, savePredictions = "final", returnResamp = "final", returnData = TRUE) # classProbs = TRUE # sampling = "none"
+
 # Using Leave-One-Out-Cross-Validation (LOOCV)
-#train_ctrl <- trainControl(method = "loocv", index = myFolds, savePredictions = "final", returnResamp = "final", returnData = TRUE)
+train_ctrl <- trainControl(method = "loocv", index = myFolds, savePredictions = "final", returnResamp = "final", returnData = TRUE)
 
